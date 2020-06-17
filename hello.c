692f94a61622771861b788afba11d8de78855243
Mohamed Khaled Abdelsamea <36470288+MohamedKhaled30@users.noreply.github.com>
2020-06-17 09:09:13 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    //prompt the user to enter his name
     string name = get_string("Whats your name?\n");
+    //print out hello and his name
     printf("hello, %s\n", name);
 }
